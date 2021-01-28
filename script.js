2fd9dc61267e3068ba721d315592a5e6c1756d37
CodeKrakken <newholm1@hotmail.com>
2021-01-28 15:47:30 +0000

    Tailor references for shopto instead of bestbuy

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const { firefox } = require('playwright');
 (async () => {
   const browser = await firefox.launch({ headless: false });
-  searchBestBuyForPS5(browser);
+  searchForPS5(browser);
 })();
 
 // helper function for sleeping
@@ -10,31 +10,31 @@ const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-// search whether PS5 is in stock at Best Buy
-var searchBestBuyForPS5 = async (browser) => {
-  // go to Best Buy's site
+// search whether PS5 is in stock
+var searchForPS5 = async (browser) => {
+  // go to site
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('https://www.argos.co.uk');
+  await page.goto('https://www.shopto.net');
 
   // enter search term "playstation 5 console"
-  await page.type('#gh-search-input', 'playstation 5 console', {delay: 100});
+  await page.type('#input_search', 'playstation 5 digital console', {delay: 100});
   await sleep(3000);
-  await page.press('.header-search-button', 'Enter');
+  await page.press('.search_button', 'Enter');
   await sleep(1000);
 
   // wait for result products to render and put them in a results array
-  await page.innerHTML('li.sku-item');
-  const results = await page.$$('li.sku-item');
+  await page.innerHTML('itemlist_container');
+  const results = await page.$$('itemlist_container');
 
   // iterate through results array
   for (let i = 0; i < results.length; i++) {
     // get product's name
-    const skuHeader = await results[i].$('h4.sku-header');
-    const html = await skuHeader.innerHTML();
+    const itemlistDescription = await results[i].$('itemlist_description');
+    const html = await itemlistDescription.innerHTML();
 
     // check whether product's name contains "playstation 5" and "console"
-    if (html.toLowerCase().includes('playstation 5') && html.toLowerCase().includes('console')) {
+    if (html.toLowerCase().includes('playseat') && html.toLowerCase().includes('evolution')) {
       // get Sold Out/Add to Cart button
       const button = await results[i].$('button.add-to-cart-button');
       const buttonText = await button.innerText();
